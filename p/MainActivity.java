a94d981e998b39d0063c04b3acf99ad97ba9b962
Sarvesh Tank <sarvesh8486@gmail.com>
2017-03-30 16:41:58 +0530

    Update MainActivity.java

--- p/MainActivity.java
+++ p/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.sarveshtank.securenotes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.example.sarveshtank.securenotes.data.DatabaseQueries;
 import com.example.sarveshtank.securenotes.util.UserListAdapters;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,9 +29,24 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView.setHasFixedSize(true);
 
-        adapters = new UserListAdapters(null);
+        adapters = new UserListAdapters(DatabaseQueries.selectNotesAll(this));
         recyclerView.setAdapter(adapters);
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.add_new_note){
+            Intent intent = new Intent(this, AddNotesData.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
